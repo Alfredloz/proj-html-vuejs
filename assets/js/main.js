0ed8f1fdc225c3a08395fb51c4f32b4732a7402a
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-19 16:30:49 +0100

    creazione header
    ho fatto anche la funzione al cambio della lingua

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,18 @@
 let app = new Vue({
     el: '#app',
     data:{
-
+        languages:[
+            {text:'english', value: 0},
+            {text:'french', value: 1},
+            {text: 'german', value: 2}
+        ],
+        flagsLang:[
+            './assets/img/en.png',
+            './assets/img/fr.png',
+            './assets/img/de.png',
+        ],
+        counter: 0,
     },
     methods: {
-
     }
 });
-console.log('ciao');
