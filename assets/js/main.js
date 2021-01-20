29fe140474b17599671005066a1c4fb82f55d7b7
Alfredo <alfredogregorio.lozano@gmail.com>
2021-01-20 16:07:49 +0100

    prima e seconda section completata

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 let app = new Vue({
     el: '#app',
     data:{
+        // logo header
+        logoHeader:'./assets/img/dark-logo.png',
         // array oggetti cambio lingua
         languages:[
             {text:'english', value: 0},
@@ -24,6 +26,31 @@ let app = new Vue({
             'fas fa-shopping-cart'
         ],
         dudeBanner: './assets/img/home-business-hero-avatar.png',
+        servicesArray:[
+            {
+                img:'./assets/img/home-business-service-slide-01-480x298.jpg',
+                title: 'consultative training',
+                description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt molestiae autem hic reiciendis quas animi!',
+                clickInfo:'discover now',
+                iconClass: 'fas fa-long-arrow-alt-right',
+            },
+            {
+                img:'./assets/img/home-business-service-slide-02-480x298.jpg',
+                title: 'real deal coaching',
+                description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt molestiae ',
+                clickInfo:'discover now',
+                iconClass: 'fas fa-long-arrow-alt-right',
+
+
+            },
+            {
+                img:'./assets/img/home-business-service-slide-03-480x298.jpg',
+                title: 'advisor training program',
+                description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt molestiae autem hic reiciendis',
+                clickInfo:'discover now',
+                iconClass: 'fas fa-long-arrow-alt-right',
+            }
+        ]
     },
     methods: {
     }
